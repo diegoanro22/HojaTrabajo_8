c9d939cc7fadc52593d86f667b966010154bf452
diegoanro22 <diegorosalesv22@gmail.com>
2024-04-03 22:36:43 -0600

    Creacion y lectura de la clase archivo

---
+++ Archivo.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+//Clase para leer el archivo
+public class Archivo {
+    private File archivo;
+
+    public Archivo(String nameArchivo){
+        archivo = new File(nameArchivo);
+    }
+
+    public ArrayList<String> leerArchivo() throws IOException {
+        ArrayList<String> listpalabras = new ArrayList<>();
+        FileReader fileReader = new FileReader(archivo);
+        BufferedReader lector = new BufferedReader(fileReader);
+
+        String linea;
+        while ((linea = lector.readLine()) != null){
+            listpalabras.add(linea.replace(", ",",").toString());
+            
+        }
+        lector.close();
+        return listpalabras;
+    }
+
+
+
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         
+        Archivo pacientestxt = new Archivo("pacientes.txt");
+        ArrayList<String> pacientes = null;
+
+        try{
+            pacientes = pacientestxt.leerArchivo();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (String paciente : pacientes){
+            String [] campos = paciente.split(",",3);
+            System.out.println(campos[0]);
+            System.out.println(campos[1]);
+        }
+
     }
 }
